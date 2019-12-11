062c2a8304384942245680ec148063514f83083d
CHANG <Eugene.CHANG@EDUCATION.GOV.UK>
2019-12-11 13:09:41 +0000

    feat: minimal dashboard example

--- InternationalStudents/server.R
+++ InternationalStudents/server.R
@@ -1,6 +1,22 @@
 server <- function(input, output) {
+  chartdata1 <- reactive({
+    sr_chart_6 %>% 
+      filter(`Country of domicile` == input$provider)
+    # data.frame(
+    #   group = c("teaching","research","other"),
+    #   value = c(input$provider[[1]],
+    #             input$provider[[1]],
+    #             input$provider[[1]])
+    # )
+  })
   
-
+  
+  output$plot1 <- renderPlot({
+    ggplot(chartdata1(),aes(x =`Academic year, y = Number)) +
+      geom_line(aes(),stat="identity")+
+      ylab(label='Student Numbers') +
+      xlab(label="Year")
+      # scale_fill_manual(values=c("orange","darkblue","darkgreen"),name = " ")
+  })
 }
 
-shinyApp(ui, server)
