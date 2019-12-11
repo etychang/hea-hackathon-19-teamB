a60b5457def206f170a40a6565b07d1ac0d03b4b
Serena <serena.abbott@education.gov.uk>
2019-12-11 11:59:50 +0000

    loaded in csv files

--- R/load_data.R
+++ R/load_data.R
@@ -1,3 +1,14 @@
 filepath <- "//Lonnetapp01/higher education analysis/HE Analysis/Economics Analysis Team/Hackathon/Data/Challenge 2/" 
 
-df <- data.table::fread(paste0(filepath), "SR_Chart_6.csv")
+sr_chart_6 <- data.table::fread(paste0(filepath, "SR_Chart_6.csv"))
+sr_figure_8 <- data.table::fread(paste0(filepath, "SR_Figure_8.csv"))
+sr_table_1 <- data.table::fread(paste0(filepath, "SR_Table_1.csv"))
+
+sr_table_28_1415 <- data.table::fread(paste0(filepath, "SR_TABLE_28_(2014-15).csv"))
+sr_table_28_1516 <- data.table::fread(paste0(filepath, "SR_TABLE_28_(2015-16).csv"))
+sr_table_28_1617 <- data.table::fread(paste0(filepath, "SR_TABLE_28_(2016-17).csv"))
+sr_table_28_1718 <- data.table::fread(paste0(filepath, "SR_TABLE_28_(2017-18).csv"))
+
+unesco_hesa_match <- data.table::fread(paste0(filepath, "UNESCO_HESA_MATCH.csv"))
+unesco_inbound_all <- data.table::fread(paste0(filepath, "UNESCO_INBOUND_ALL.csv"))
+unesco_inbound_country <- data.table::fread(paste0(filepath, "UNESCO_INBOUND_COUNTRY.csv"))
