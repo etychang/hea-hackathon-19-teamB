e19e345f1b1e6ed9d52c75938d254dbeab97fd9e
CHANG <Eugene.CHANG@EDUCATION.GOV.UK>
2019-12-11 12:27:53 +0000

    feat: added skeleton for RShiny App

---
+++ InternationalStudents/ui.R
@@ -0,0 +1,32 @@
+ui <- dashboardPage(
+  dashboardHeader(title="Provider Metrics"),
+  dashboardSidebar(),
+  dashboardBody(
+    selectInput("provider",
+                label = "Choose a provider to examine",
+                choices = 1:5,
+                selected = "Anglia Ruskin University"),
+    fluidRow(
+      valueBoxOutput("value1", width = 4),
+      valueBoxOutput("value2", width = 4),
+      valueBoxOutput("value3", width = 4),
+      box(title = "Income Breakdown",solidHeader=TRUE,status="primary",plotOutput("plot1", height = 250)),
+      valueBox(paste0(6,"%"), "Cash flow", icon = icon("credit-card"), color = "yellow"),
+      
+      
+      box(title = "cash flow chart", solidHeader=TRUE,width = 4,
+          plotOutput("plot3",height = 250)),
+      
+      
+      box(title = "Total Income", width = 4,
+          plotOutput("plot4" ,height = 250)),
+      
+      
+      valueBox(paste0(6,"%"), "External borrowing", icon = icon("bitcoin"), color = "yellow"),
+      
+      box(tableOutput('table'),title = "Cost Breakdown", width = 4),
+      
+      box(title = "Spider diagram", width = 4,
+          plotOutput("plot5", height = 250))
+    )
+  ))
